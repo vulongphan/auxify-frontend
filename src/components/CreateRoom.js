9e6897128f76a93c512e927613346fdfcfbb8e41
Phan Vu Long <vulongphan@Phans-MacBook-Air.local>
2020-08-23 18:34:22 +0400

    update Expire page

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -13,7 +13,9 @@ class CreateRoom extends React.Component {
                     </form>
                 </div>
 
-                <p className="fromtop text_style">You need a Spotify Premium account to host a new room</p>
+                <p className="fromtop text_style">You need a Spotify Premium Account to host a new room</p>
+                <p className="fromtop text_style">We recommend that you host a room by using your computer or laptop </p>
+
             </div>
         )
     }
