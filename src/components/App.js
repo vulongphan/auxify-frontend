b6adf68e34a206af4d09afa516b18ac7cc206ad3
Phan Vu Long <vulongphan@Phans-MacBook-Air.local>
2020-09-01 19:35:27 +0400

    add Maintainance Component

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,39 @@ import About from './About';
 import CreateRoom from './CreateRoom';
 import JoinRoom from './JoinRoom';
 import Expire from './Expire';
+import Maintainance from './Maintainance'
+
+var maintainance = false; //defaultly we dont need maintainance
 
 class App extends React.Component {
   render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/room' component={Room} />
-            <Route path='/create' component={CreateRoom} />
-            <Route path='/join' component={JoinRoom} />
-            <Route path='/expire' component={Expire} />
-          </Switch>
-        </div>
-      </Router>
-    )
+    if (maintainance === false) { //if no maintainance if needed
+      return (
+        <Router>
+          <div>
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/room' component={Room} />
+              <Route path='/create' component={CreateRoom} />
+              <Route path='/join' component={JoinRoom} />
+              <Route path='/expire' component={Expire} />
+            </Switch>
+          </div>
+        </Router>
+      )
+    }
+    else { //if maintainance is needed
+      return (
+        <Router>
+          <div>
+            <Switch>
+              <Route path = '/' exact component = {Maintainance} /> 
+            </Switch>
+          </div>
+        </Router>
+      )
+    }
   }
 }
 
@@ -31,16 +47,16 @@ class Home extends React.Component {
   render() {
     return (
       <div className="homePage center" >
-        <h1 className="fromtop text_style" id = "welcome" >Welcome to Auxify</h1>
+        <h1 className="fromtop text_style" id="welcome" >Welcome to Auxify</h1>
         <div className="btn-group fromtop" >
           <form action='/create'>
-            <button type="submit" className = "roomAction" >New Room</button>
+            <button type="submit" className="roomAction" >New Room</button>
           </form>
           <form action='/join'>
-            <button type="submit" className = "roomAction">Join Room</button>
+            <button type="submit" className="roomAction">Join Room</button>
           </form>
         </div>
-        <p className = "frombottom"><u><Link to = {`/about`}>About</Link></u></p>
+        <p className="frombottom"><u><Link to={`/about`}>About</Link></u></p>
       </div>
     )
   }
